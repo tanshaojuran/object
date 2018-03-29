90cb686303fd2f4a8286b645cd6f217f0ca3ab21
tan107192 <107192@qq.com>
2018-03-29 10:35:40 +0800

    控制层

---
+++ twilight/module/config.js
@@ -0,0 +1,21 @@
+/**
+ * Created by asus-pc on 2018/3/12.
+ */
+const mysql=require('mysql');
+module.exports={
+    query:function(sql,arr,backnum){
+        const myconnect=mysql.createConnection({
+            host:'localhost',
+            user:'root',
+            password:'root',
+            port:'3308',
+            database:''
+        });
+        //打开连接
+        myconnect.connect();
+        //数据库操作
+        myconnect.query(sql,arr,backnum);
+        //关闭连接
+        myconnect.end;
+    }
+}
---
+++ twilight/module/sqlcools.js
@@ -0,0 +1,25 @@
+/**
+ * Created by asus-pc on 2018/3/12.
+ */
+module.exports.sqlpool=function(){
+    let pool={
+        config:{
+            host:'localhost',
+            user:'root',
+            password:'root',
+            port:'3308',
+            database:''
+        },
+        connect:function(sql,arr,fn){
+            const pool=mysql.createPool(this.config);
+            pool.getConnection(function(err,connect){
+                if(err){
+                    console.log(err)
+                }else{
+                    connect.query(sql,arr,fn);
+                }
+            })
+        }
+    }
+}
+
