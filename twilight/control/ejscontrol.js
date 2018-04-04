db7c829ac37fee4a25f4e9d6deb80e9cce805961
tan107192 <107192@qq.com>
2018-04-04 15:28:51 +0800

    ejscontrol.js

---
+++ twilight/control/ejscontrol.js
@@ -0,0 +1,9 @@
+/**
+ * Created by asus-pc on 2018/4/3.
+ */
+const ejsmodule=require('./../module/ejsmodule.js')
+module.exports={
+    indexlist:function(req,res){
+            res.render('index.html');
+    }
+};
