e7a5e8d8d7db709615fa6a22afe7a333a0b2bbc2
tanshaojuran <1071923745@qq.com>
2018-04-07 17:52:18 +0800

    sqlpool.js错误更改--杨玉梅

---
+++ twilight/module/sqlpool.js
@@ -0,0 +1,35 @@
+/**
+ * Created by asus-pc on 2018/3/12.
+ */
+const mysql = require("mysql");//引用mysql文件
+module.exports.sqlpool=function(){
+    let pool={
+        config:{
+            //公共数据库
+            host:'172.16.13.115',
+            user:'root',
+            password:'root',
+            port:'3306',
+            database:'twilight'
+            //本机测试数据库
+            //host:"localhost",
+            //user:"root",
+            //password:"root",
+            //port:3306,
+            //database:"twilight"
+        },
+        connect:function(sql,arr,fn){
+            //1.创建一个连接池对象
+            const pool=mysql.createPool(this.config);
+            //2.获取连接对象
+            pool.getConnection(function(err,connect){
+                if(err){
+                    console.log(err);
+                }else{
+                    connect.query(sql,arr,fn)
+                }
+            });
+        }
+    };
+    return pool;
+};
