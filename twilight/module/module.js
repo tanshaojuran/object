316d147ee256032ea0c9c720f49cd397860b2631
tanshaojuran <1071923745@qq.com>
2018-04-07 17:59:27 +0800

    module.js文件中sqlpool.js引用路径更改--杨玉梅

--- twilight/module/module.js
+++ twilight/module/module.js
@@ -1,7 +1,16 @@
 /**
  * Created by asus-pc on 2018/4/3.
  */
-const mysql=require('mysql');
-const lj=require('./config.js');
+const mysql = require("mysql");//引用mysql文件
+const sqlpool = require("./sqlpool.js");//引用sqlpool文件
+const db = sqlpool.sqlpool();//执行sqlpoll函数
 module.exports={
+    //Productlist产品列表页的module  start
+    ProListSearch:function(callback){
+        let sql="select * from product_list where 1=1";
+        let arr=[];
+        db.connect(sql,arr,callback);
+
+    }
+    //Productlist产品列表页的module  end
 };
