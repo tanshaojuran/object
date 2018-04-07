97743877a9f50d909da84a8fe77670fb01c5a30f
tanshaojuran <1071923745@qq.com>
2018-04-07 18:30:20 +0800

    control.js修改， ProductListControl.js文件（产品列表页）添加--杨玉梅

---
+++ twilight/control/ProductListControl.js
@@ -0,0 +1,16 @@
+/**
+ * Created by 杨玉梅 on 2018/4/7.
+ * Productlist产品列表页的路由
+ */
+const usermodule=require('./../module/ProductListModule.js')//引用ProductListModule.js文件
+module.exports={
+    ProListShangPin:function(req,res){
+        let a=req.body.theName;
+        console.log(a);
+        usermodule.ProListSearch(function(err,data){
+            console.log(data)
+            //res.send(data);
+        });
+    }
+};
+
--- twilight/control/control.js
+++ twilight/control/control.js
@@ -1,7 +1,7 @@
 /**
  * Created by asus-pc on 2018/3/12.
  */
-const usermodule=require('./../module/module.js')
+const usermodule=require('./../module/module.js')//引用module.js文件
 module.exports={
 
 };
