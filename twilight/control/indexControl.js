910701e89a32c0fb78ea1790f50baf909c3bfec2
tanshaojuran <1071923745@qq.com>
2018-04-10 15:37:53 +0800

    indexControl.js添加--吴霜

---
+++ twilight/control/indexControl.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2018/4/10 0010.
+ */
+const myModal = require('./../module/indexModule.js');
+
+module.exports={
+    indexPage:function(req,res){
+        res.render('index.html');
+    }
+};
