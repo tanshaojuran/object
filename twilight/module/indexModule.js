6e9cb6e44dda5fbe73d2a25d35262966a7690146
tanshaojuran <1071923745@qq.com>
2018-04-10 15:39:32 +0800

    indexModule.js添加--吴霜

---
+++ twilight/module/indexModule.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2018/4/10 0010.
+ */
+const mysql = require("mysql");//引用mysql文件
+const sqlpool = require("./sqlpool.js");//引用sqlpool文件
+const db = sqlpool.sqlpool();//执行sqlpoll函数
+
+module.exports={
+
+};
