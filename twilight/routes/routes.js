5d8ed5b0825b6d5345600e4a600ae8e9c4026aed
tanshaojuran <1071923745@qq.com>
2018-04-10 15:34:11 +0800

    index--吴霜

--- twilight/routes/routes.js
+++ twilight/routes/routes.js
@@ -5,10 +5,16 @@ const  myexpress=require('express');//引用express框架
 const myroutes=myexpress.Router();//使用express的Route类来进行路由的创建
 const usercontrol=require('./../control/control.js');//引用usercontrol.js文件
 const ProductListControl=require('./../control/ProductListControl.js');//引用ProductListControl.js文件，产品列表页
+const IndexControl = require('./../control/indexControl.js')//引用indexControl文件
 
 //Productlist产品列表页的路由发送  start
 myroutes.post("/ProListShangPin.do",ProductListControl.ProListShangPin);
 //Productlist产品列表页的路由发送  end
+
+//index首页的路由发送  start
+myroutes.get('/index.html',IndexControl.indexPage);
+//index首页的路由发送  end
+
 
 module .exports=myroutes;
 
