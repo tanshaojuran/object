96dd8809dd6730166877f2e9d893532b321e8d42
tanshaojuran <1071923745@qq.com>
2018-04-10 11:29:33 +0800

    页面路径更改--杨玉梅

--- twilight/public/js/ProductList.js
+++ twilight/public/js/ProductList.js
@@ -9,20 +9,20 @@ $(function(){
         ProListdiv.css({"border":"4px solid #f8f8f8","box-shadow":"0px 0px 0px #888"});
         $(this).css({"border":"4px solid #ffffff","box-shadow":"0px 0px 4px #888"});
         if($(this).index()==0){
-            $("#ProListS1Center img").attr({ src: "../images/ycxl_dt_01.jpg", alt: "大图1" });
-            $("#prolistS1Right div div img").attr({ src: "../images/ycxl_zst_01.jpg", alt: "展示图1" });
+            $("#ProListS1Center img").attr({ src: "images/ycxl_dt_01.jpg", alt: "大图1" });
+            $("#prolistS1Right div div img").attr({ src: "images/ycxl_zst_01.jpg", alt: "展示图1" });
             $("#prolistS1Right div h2").text("北欧餐厅长形吊灯");
             $("#prolistS1Right div h3").text("A Nordic restaurant");
             $("#prolistS1Right div p span").text("475.00");
         }else if($(this).index()==1){
-            $("#ProListS1Center img").attr({ src: "../images/ycxl_dt_02.jpg", alt: "大图2" });
-            $("#prolistS1Right div div img").attr({ src: "../images/ycxl_zst_02.jpg", alt: "展示图2" });
+            $("#ProListS1Center img").attr({ src: "images/ycxl_dt_02.jpg", alt: "大图2" });
+            $("#prolistS1Right div div img").attr({ src: "images/ycxl_zst_02.jpg", alt: "展示图2" });
             $("#prolistS1Right div h2").text("北欧餐厅圆形吊灯");
             $("#prolistS1Right div h3").text("A Nordic restaurant");
             $("#prolistS1Right div p span").text("550.00");
         }else if($(this).index()==2){
-            $("#ProListS1Center img").attr({ src: "../images/ycxl_dt_03.jpg", alt: "大图3" });
-            $("#prolistS1Right div div img").attr({ src: "../images/ycxl_zst_03.jpg", alt: "展示图3" });
+            $("#ProListS1Center img").attr({ src: "images/ycxl_dt_03.jpg", alt: "大图3" });
+            $("#prolistS1Right div div img").attr({ src: "images/ycxl_zst_03.jpg", alt: "展示图3" });
             $("#prolistS1Right div h2").text("北欧客厅长形吊灯");
             $("#prolistS1Right div h3").text("A Nordic living room");
             $("#prolistS1Right div p span").text("355.00");
@@ -58,44 +58,44 @@ $(function(){
             $($(".ProListMenu li h2")[$(this).index()]).css({"color":"#09897d"});
             $($(".ProListMenu li p")[$(this).index()]).css({"color":"#09897d"});
             if($(this).index()==0){
-                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_01.png"})
+                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_01.png"})
             }
             if($(this).index()==1){
-                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_02.png"})
+                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_02.png"})
             }
             if($(this).index()==2){
-                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_03.png"})
+                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_03.png"})
             }
             if($(this).index()==3){
-                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_04.png"})
+                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_04.png"})
             }
             if($(this).index()==4){
-                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_05.png"})
+                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_05.png"})
             }
             if($(this).index()==5){
-                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_06.png"})
+                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_06.png"})
             }
             //鼠标移出事件
             $(".ProListMenu li").on("mouseout" ,function() {
                 $($(".ProListMenu li h2")[$(this).index()]).css({"color":"#000"});
                 $($(".ProListMenu li p")[$(this).index()]).css({"color":"#000"});
                 if($(this).index()==0){
-                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_01.png"})
+                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_01.png"})
                 }
                 if($(this).index()==1){
-                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_02.png"})
+                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_02.png"})
                 }
                 if($(this).index()==2){
-                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_03.png"})
+                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_03.png"})
                 }
                 if($(this).index()==3){
-                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_04.png"})
+                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_04.png"})
                 }
                 if($(this).index()==4){
-                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_05.png"})
+                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_05.png"})
                 }
                 if($(this).index()==5){
-                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_06.png"})
+                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_06.png"})
                 }
             });
         });
@@ -106,55 +106,55 @@ $(function(){
         //还原最初样式
         $(".ProListMenu li h2").css({"color":"#000"});
         $(".ProListMenu li p").css({"color":"#000"});
-        $($(".ProListMenu li div img")[0]).attr({ src: "../images/icon_01.png"});
-        $($(".ProListMenu li div img")[1]).attr({ src: "../images/icon_02.png"});
-        $($(".ProListMenu li div img")[2]).attr({ src: "../images/icon_03.png"});
-        $($(".ProListMenu li div img")[3]).attr({ src: "../images/icon_04.png"});
-        $($(".ProListMenu li div img")[4]).attr({ src: "../images/icon_05.png"});
-        $($(".ProListMenu li div img")[5]).attr({ src: "../images/icon_06.png"});
+        $($(".ProListMenu li div img")[0]).attr({ src: "images/icon_01.png"});
+        $($(".ProListMenu li div img")[1]).attr({ src: "images/icon_02.png"});
+        $($(".ProListMenu li div img")[2]).attr({ src: "images/icon_03.png"});
+        $($(".ProListMenu li div img")[3]).attr({ src: "images/icon_04.png"});
+        $($(".ProListMenu li div img")[4]).attr({ src: "images/icon_05.png"});
+        $($(".ProListMenu li div img")[5]).attr({ src: "images/icon_06.png"});
         //鼠标移入事件
         ProListMenuOver();
         $(this).on("mouseover",function(){
             $($(".ProListMenu li h2")[$(this).index()]).css({"color":"#09897d"});
             $($(".ProListMenu li p")[$(this).index()]).css({"color":"#09897d"});
             if($(this).index()==0){
-                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_01.png"})
+                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_01.png"})
             }
             if($(this).index()==1){
-                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_02.png"})
+                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_02.png"})
             }
             if($(this).index()==2){
-                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_03.png"})
+                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_03.png"})
             }
             if($(this).index()==3){
-                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_04.png"})
+                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_04.png"})
             }
             if($(this).index()==4){
-                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_05.png"})
+                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_05.png"})
             }
             if($(this).index()==5){
-                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_06.png"})
+                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_06.png"})
             }
             $(this).on("mouseout",function(){
                 $($(".ProListMenu li h2")[$(this).index()]).css({"color":"#09897d"});
                 $($(".ProListMenu li p")[$(this).index()]).css({"color":"#09897d"});
                 if($(this).index()==0){
-                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_01.png"})
+                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_01.png"})
                 }
                 if($(this).index()==1){
-                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_02.png"})
+                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_02.png"})
                 }
                 if($(this).index()==2){
-                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_03.png"})
+                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_03.png"})
                 }
                 if($(this).index()==3){
-                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_04.png"})
+                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_04.png"})
                 }
                 if($(this).index()==4){
-                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_05.png"})
+                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_05.png"})
                 }
                 if($(this).index()==5){
-                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_06.png"})
+                    $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_06.png"})
                 }
             });
 
@@ -164,54 +164,54 @@ $(function(){
             $($(".ProListMenu li h2")[$(this).index()]).css({"color":"#09897d"});
             $($(".ProListMenu li p")[$(this).index()]).css({"color":"#09897d"});
             if($(this).index()==0){
-                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_01.png"})
+                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_01.png"})
             }
             if($(this).index()==1){
-                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_02.png"})
+                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_02.png"})
             }
             if($(this).index()==2){
-                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_03.png"})
+                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_03.png"})
             }
             if($(this).index()==3){
-                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_04.png"})
+                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_04.png"})
             }
             if($(this).index()==4){
-                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_05.png"})
+                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_05.png"})
             }
             if($(this).index()==5){
-                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_06.png"})
+                $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_06.png"})
             }
         });
         //改变点击的li的样式
         $(this).find("h2").css({"color":"#09897d"});
         $(this).find("p").css({"color":"#09897d"});
         if($(this).index()==0){
-            $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_01.png"});
+            $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_01.png"});
             $('.slider-input').jRange('setValue', '0,3000');
             ProListAjax({"className":"全部","price":$(".slider-input").val()});
         }
         if($(this).index()==1){
-            $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_02.png"});
+            $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_02.png"});
             $('.slider-input').jRange('setValue', '0,3000');
             ProListAjax({"className":"吊灯","price":$(".slider-input").val()});
         }
         if($(this).index()==2){
-            $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_03.png"});
+            $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_03.png"});
             $('.slider-input').jRange('setValue', '0,3000');
             ProListAjax({"className":"壁灯","price":$(".slider-input").val()});
         }
         if($(this).index()==3){
-            $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_04.png"});
+            $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_04.png"});
             $('.slider-input').jRange('setValue', '0,3000');
             ProListAjax({"className":"台灯","price":$(".slider-input").val()});
         }
         if($(this).index()==4){
-            $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_05.png"});
+            $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_05.png"});
             $('.slider-input').jRange('setValue', '0,3000');
             ProListAjax({"className":"落地灯","price":$(".slider-input").val()});
         }
         if($(this).index()==5){
-            $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "../images/icon_djs_06.png"});
+            $($(".ProListMenu li div img")[$(this).index()]).attr({ src: "images/icon_djs_06.png"});
             $('.slider-input').jRange('setValue', '0,3000');
             ProListAjax({"className":"吸顶灯","price":$(".slider-input").val()});
         }
@@ -243,7 +243,7 @@ $(function(){
     }
     //收藏,动态绑定点击事件
     $("#ProListS4Ul").on("click","li div .ProListXin",function(e){
-        e.stopPropagation();
+        e.stopPropagation();//阻止冒泡
         if($(this).attr("ProListSC")=="f"){
             $(this).html("&#xe684;");
             $(this).attr("ProListSC","t")
@@ -254,13 +254,13 @@ $(function(){
     });
     //购物车，动态绑定点击事件
     $("#ProListS4Ul").on("click","li div .ProListCar",function(e){
-        e.stopPropagation();
+        e.stopPropagation();//阻止冒泡
     });
 
     //点击跳转商品详情页面,动态绑定点击事件
     $("#ProListS4Ul").on("click","li",function(){
         var ProductID=$(this).attr("ProductID");
-        location.href ="login.html?ProductData="+ProductID;
+        location.href ="login.html?ProductID="+ProductID;
     });
 
 });
