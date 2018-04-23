d2c916f6cff28e00c7212d147aaab53e9f164e0a
tan107192 <107192@qq.com>
2018-04-24 07:53:57 +0800

    登录路由

--- twilight/routes/routes.js
+++ twilight/routes/routes.js
@@ -7,6 +7,11 @@ const usercontrol=require('./../control/control.js');//引用usercontrol.js文件
 const ProductListControl=require('./../control/ProductListControl.js');//引用ProductListControl.js文件，产品列表页
 const IndexControl = require('./../control/indexControl.js')//引用indexControl文件
 
+myroutes.post('/login.do',usercontrol.userlogin);//登录
+
+myroutes.post('/sendCode.do',usercontrol.sendCode);//发送验证码
+
+myroutes.post('/reg.do',usercontrol.reg);//注册验证
 
 //Productlist产品列表页的路由发送  start
 myroutes.post("/ProListShangPin.do",ProductListControl.ProListShangPin);
@@ -15,10 +20,6 @@ myroutes.post("/ProListShangPin.do",ProductListControl.ProListShangPin);
 //index首页的路由发送  start
 myroutes.get('/index.html',IndexControl.indexPage);
 //index首页的路由发送  end
-myroutes.post('/sendCode.do',usercontrol.sendCode);
-//发送验证码
-myroutes.post('/reg.do',usercontrol.reg);
-//注册验证
 module .exports=myroutes;
 
 
